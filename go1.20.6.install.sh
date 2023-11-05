4d559e6c3f8828c85fcd1e4ba66e6e04344d06a9
maxliulc <153888343@qq.com>
2023-11-05 17:20:37 +0800

    Update go1.20.6.install.sh
    修改错误

--- go1.20.6.install.sh
+++ go1.20.6.install.sh
@@ -3,14 +3,9 @@
 wget https://golang.google.cn/dl/go1.20.6.linux-amd64.tar.gz      #安装
 tar -zxvf go1.20.6.linux-amd64.tar.gz -C /usr/local/              #解压
 
-if [[ -f ".bashrc" ]]; then
-    echo "PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc            #将go所在目录添加到环境变量
-    source ~/.bashrc
-fi
-if [[ -f ".zshrc" ]]; then
-    echo "PATH=\$PATH:/usr/local/go/bin" >> ~/.zshrc             #将go所在目录添加到环境变量
-    source ~/.zshrc
-fi
+echo "PATH=\$PATH:/usr/local/go/bin" >> /etc/profile
+echo "PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
+source /etc/profile
 
 #go版本
 go version
